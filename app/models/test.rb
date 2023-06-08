75f66e03af2dfba9e65737f9b4f34820099fc87a
Lyambda <valer4sivaev234@gmail.com>
2023-06-08 16:42:32 +0300

    commit1

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,11 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :test_passages
+  has_many :users, through: :test_passages
+  has_many :questions
+
+  def self.sorted_tests_by_category(category_name)
+    tests = Test.joins(:category).where(categories: { name: category_name })
+    tests.order(title: :desc).pluck(:title)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Создаем пользователей
+user1 = User.create(name: 'John', email: 'john@example.com', password: '123456')
+user2 = User.create(name: 'Kate', email: 'kate@example.com', password: '123456')
+user3 = User.create(name: 'Mike', email: 'mike@example.com', password: '123456')
+
+# Создаем категории
+category1 = Category.create(name: 'History')
+category2 = Category.create(name: 'Math')
+
+# Создаем тесты
+test1 = Test.create(title: 'World War II', level: 2, category: category1)
+test2 = Test.create(title: 'Algebra', level: 3, category: category2)
+test3 = Test.create(title: 'US Presidents', level: 1, category: category1)
+
+# Создаем вопросы
+question1 = Question.create(body: 'When did WWII begin?', test: test1)
+question2 = Question.create(body: 'What country was not involved in WWII?', test: test1)
+question3 = Question.create(body: 'What is the Pythagorean theorem?', test: test2)
+question4 = Question.create(body: 'Who was the first US President?', test: test3)
+
+# Создаем ответы
+Answer.create(body: '1939', question: question1, correct: true)
+Answer.create(body: 'Japan', question: question2, correct: false)
+Answer.create(body: 'a2+b2=c2', question: question3, correct: true)
+Answer.create(body: 'George Washington', question: question4, correct: true)
